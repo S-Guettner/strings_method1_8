294193e51e5a9d2a114540fd2128836b41336db0
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-24 14:00:25 +0100

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,17 +20,11 @@ console.log(firstLastUpper);
 let middleUpper = `${firstUpper.toLowerCase()} ${middle.toUpperCase()} ${lastUpper.toLowerCase()}`
 console.log(middleUpper);
 
-/* let letterI = changedText.charAt(4).toUpperCase();
-console.log(letterI);
-let letterG = changedText.charAt(7).toUpperCase();
-console.log(letterG);
-let letterT = changedText.charAt(13).toUpperCase();
-console.log(letterT);
- */
+
 let firstCharUpper = text.slice(0,3) + ' ' + text.slice(4, 5).toUpperCase() + text.slice(5, 6) + ' ' + text.slice(7, 8).toUpperCase() + text.slice(8, 12) + ' ' + text.slice(13, 14).toUpperCase() + text.slice(14, 15) + ' ' + text.slice(16, 17).toUpperCase() + text.slice(17,23);
 console.log(firstCharUpper);
 
-
+document.write(upperCase + "<br>" + lowerCase + "<br>" + firstLastUpper +"<br>" + middleUpper + "<br>" + firstCharUpper )
 
 
 
